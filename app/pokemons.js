fedd8a7b3411e7298e89a91953a2027bdc612369
jilink <jimmyfac@gmail.com>
2022-02-03 10:49:00 +0100

    getting random pokemon

---
+++ app/pokemons.js
@@ -0,0 +1,9 @@
+const TOTAL_POKEMONS = 800;
+
+const getRandomPokemon = () => {
+  const randomIndex = Math.floor(Math.random() * TOTAL_POKEMONS);
+  return fetch(`https://pokeapi.co/api/v2/pokemon/${randomIndex}`)
+    .then(response => response.json())
+}
+
+export { getRandomPokemon };
--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,12 +1,23 @@
-import { Link, Outlet } from "remix";
+import { Link, Outlet, useLoaderData } from "remix";
+import {getRandomPokemon } from "~/pokemons"
+
 
 import { ChakraProvider, Text } from '@chakra-ui/react'
 
+export const loader = async () => {
+ return getRandomPokemon();
+};
+
 export default function App() {
+  const pokemon = useLoaderData();
+    console.log(pokemon);
   return (
     <div>
       <ChakraProvider>
-        <Text fontSize="4xl" textAlign="center">Guess the pokemon type!</Text>
+        <Text fontSize="4xl" textAlign="center">
+          Guess the pokemon type!
+        </Text>
+          <Text key={pokemon.name}>{pokemon.name}</Text>
         <Outlet />
       </ChakraProvider>
     </div>
